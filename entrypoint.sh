f603b9e7d29148f1ec3305e488423fca094f0eb2
Carlos Guajardo <cguajardo@imed.cl>
2025-06-18 15:15:45 -0400

    chore: report content is encoded as base64

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ echo "::endgroup::"
 if [ -f $REPORT_PATH ]; then
   {
     echo "success=true"
-    echo "report_content=$(cat $REPORT_PATH | sed 's/"/\\"/g')"  # Escape quotes for JSON compatibility
+    echo "report_content=\"$(cat $REPORT_PATH | base64)\""
     echo "report_path=$REPORT_PATH"
     
     # non-zero exit code means leaks found
